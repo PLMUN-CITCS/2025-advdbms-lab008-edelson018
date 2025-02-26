7b5c83a391e60fbf94dad5d45c8d0de5d05a7ebd
edelson <enriquezedelson_bsit@plmun.edu.ph>
2025-02-26 22:27:49 +0800

    Submitting ADVDBMS Week 02 - Session 01 - Exercise 05

--- bookstore_database/query_bookstore_data.sql
+++ bookstore_database/query_bookstore_data.sql
@@ -4,16 +4,20 @@ FROM `Customers` `c`
 JOIN `Orders` `o` ON `c`.`CustomerID` = `o`.`CustomerID`
 JOIN `OrderDetails` `od` ON `o`.`OrderID` = `od`.`OrderID`
 JOIN `Books` `b` ON `od`.`ISBN` = `b`.`ISBN`;
+
 UPDATE `Books` SET `Price` = 8.99 WHERE `ISBN` = '9781234567890';
 
 SELECT * FROM `Books` WHERE `ISBN` = '9781234567890';
+
 DELETE FROM `OrderDetails` WHERE `OrderID` = 1 AND `ISBN` = '9780321765723';
 
 SELECT * FROM `OrderDetails` WHERE `OrderID` = 1;
+
 SELECT `a`.`Name` AS `AuthorName`, `b`.`Title` AS `BookTitle`
 FROM `Authors` `a`
 JOIN `BookAuthors` `ba` ON `a`.`AuthorID` = `ba`.`AuthorID`
 JOIN `Books` `b` ON `ba`.`ISBN` = `b`.`ISBN`;
+
 SELECT `c`.`Name` AS `CustomerName`
 FROM `Customers` `c`
 JOIN `Orders` `o` ON `c`.`CustomerID` = `o`.`CustomerID`
